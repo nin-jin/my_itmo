305243fed506e98be7f1ad2631830632b5cc723b
nin-jin <nin-jin@users.noreply.github.com>
2024-12-08 07:14:55 +0000

    Deploying to gh-pages from @ nin-jin/my_itmo@cb33a64a938319198aa19bbf8d140b6f4a8ce0c9 🚀

--- node.mjs
+++ node.mjs
@@ -7690,7 +7690,7 @@ var $;
             display: 'block',
             position: 'relative',
             font: {
-                family: 'monospace',
+                family: 'inherit',
             },
             Numb: {
                 textAlign: 'right',
--- web.js
+++ web.js
@@ -7116,7 +7116,7 @@ var $;
             display: 'block',
             position: 'relative',
             font: {
-                family: 'monospace',
+                family: 'inherit',
             },
             Numb: {
                 textAlign: 'right',
