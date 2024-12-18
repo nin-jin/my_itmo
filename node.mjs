9c1ca8ab9566bb9246d43ece91a8c42c2c61afe1
nin-jin <nin-jin@users.noreply.github.com>
2024-12-18 07:15:55 +0000

    Deploying to gh-pages from @ nin-jin/my_itmo@cb33a64a938319198aa19bbf8d140b6f4a8ce0c9 🚀

--- node.mjs
+++ node.mjs
@@ -4549,7 +4549,8 @@ var $;
                     stabilityThreshold: 100,
                 },
             });
-            watcher.on('all', (type, path) => {
+            watcher
+                .on('all', (type, path) => {
                 if (path instanceof Error) {
                     this.$.$mol_fail_log(path);
                     return;
@@ -4562,7 +4563,8 @@ var $;
                 else {
                     file.parent().reset();
                 }
-            });
+            })
+                .on('error', $mol_fail_log);
             return {
                 destructor() {
                     watcher.close();
