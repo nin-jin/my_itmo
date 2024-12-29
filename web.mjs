fbf88cc6c277685fcbb66d2f8b2137ee21e317d7
nin-jin <nin-jin@users.noreply.github.com>
2024-12-29 07:13:38 +0000

    Deploying to gh-pages from @ nin-jin/my_itmo@cb33a64a938319198aa19bbf8d140b6f4a8ce0c9 🚀

--- web.mjs
+++ web.mjs
@@ -5109,7 +5109,7 @@ var $;
             Tools: {
                 flex: {
                     basis: 'auto',
-                    grow: 1000,
+                    grow: 0,
                     shrink: 1,
                 },
                 display: 'flex',
