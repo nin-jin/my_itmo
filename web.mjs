7d429664ef07834991f0457aef31036f711580b3
nin-jin <nin-jin@users.noreply.github.com>
2025-01-01 07:15:05 +0000

    Deploying to gh-pages from @ nin-jin/my_itmo@cb33a64a938319198aa19bbf8d140b6f4a8ce0c9 🚀

--- web.mjs
+++ web.mjs
@@ -5072,7 +5072,6 @@ var $;
             maxHeight: per(100),
             boxSizing: 'border-box',
             color: $mol_theme.text,
-            backdropFilter: blur(`3px`),
             ':focus': {
                 outline: 'none',
             },
